e004efb1c3805cae61458a894ca8ffc68b03fb92
VCHINIMI <vinaychinimilli@gmail.com>
2020-09-25 21:31:25 +0530

    [VINAY]Added: UC1

---
+++ EmployeeWage.java
@@ -0,0 +1,13 @@
+
+public class EmployeeWage {
+	public static void main(String args[]){
+		int FULL_TIME = 1;
+		
+		double empCheck= Math.floor(Math.random() *10)%2;
+		if( empCheck == FULL_TIME )
+			System.out.println("present");
+		else
+			System.out.println("absent");
+	}
+
+}
